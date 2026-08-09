6152b808af1bcc9eb2859cd3586bec8a5388ca79
aimen <aimenfaidi28@gmail.com>
2026-08-10 00:16:37 +0200

    fix: execution in a pod from the controller

--- internal/controller/lrp_reconciler.go
+++ internal/controller/lrp_reconciler.go
@@ -38,6 +38,13 @@ type LRPReconciler struct {
 	RESTConfig *rest.Config
 }
 
+// +kubebuilder:rbac:groups=cilium.io,resources=ciliumlocalredirectpolicies,verbs=get;list;watch;update;patch
+// +kubebuilder:rbac:groups="",resources=pods,verbs=get;list;watch;delete
+// +kubebuilder:rbac:groups="",resources=pods/exec,verbs=create;get
+// +kubebuilder:rbac:groups="",resources=nodes;namespaces;services,verbs=get;list;watch
+// +kubebuilder:rbac:groups=coordination.k8s.io,resources=leases,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups="",resources=events,verbs=create;patch
+
 func (r *LRPReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := log.FromContext(ctx)
 
